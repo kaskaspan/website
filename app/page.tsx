5a3eaccbcdf0aebe5f486674318837984c0fd53e
Kasper Pan <kasper@Kaspers-MacBook-Pro.local>
2025-10-09 15:59:30 +0800

    add solgn

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Kasper website</h1>
+        <h1 className="text-4xl font-bold">Kasper</h1>
+        <p className="text-lg">I&apos;m building my world.</p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
