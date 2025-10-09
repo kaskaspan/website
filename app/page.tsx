8c45c38f679de86fcf0e8cb778660b3860474694
Kasper Pan <kasper@Kaspers-MacBook-Pro.local>
2025-10-09 17:11:57 +0800

    Revert "add a new page"
    This reverts commit 77443a012fd7d808ea3c004f63c3c0cc9878ab10.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,11 +6,6 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">Kasper</h1>
         <p className="text-lg">I&apos;m building my world.</p>
-        <Link href="/about">
-          <button className="px-6 py-3 bg-blue-400 text-white rounded-lg hover:bg-blue-700 transition-colors font-medium">
-            Go to About Page
-          </button>
-        </Link>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
